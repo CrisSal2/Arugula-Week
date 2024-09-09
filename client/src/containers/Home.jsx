6c6d0b27618d39d7c84b290d3463e1e18c3666ab
hweltzien <hmweltzien@gmail.com>
2024-09-09 10:11:58 -0700

    remove default contents of App.css add background image and sign-in button to landing page

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 function Home() {
     return(
         <div className="Home">
-            Landing Page
+            <div className="lander">
+                <h1>Sign in to Get Started</h1>
+                <Link className="sign-in-btn" to="/sign-in">Sign in</Link>
+                </div>
         </div>
     )
 }
