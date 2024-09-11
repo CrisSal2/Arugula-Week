aca2c359dc47402a049832a6a63408616a7b257e
hweltzien <hmweltzien@gmail.com>
2024-09-11 13:30:16 -0700

    my week containers added

--- client/src/containers/MyWeek.jsx
+++ client/src/containers/MyWeek.jsx
@@ -3,7 +3,17 @@ import React from "react"
 function MyWeek() {
     return(
         <div className="My-Week">
-            My Week
+            <div className="grid grid-cols-5 gap-4">
+            <div className="dayColumn">Monday
+            <div className="mealColumn">Breakfast</div>
+            <div className="mealColumn">Lunch</div>
+            <div className="mealColumn">Dinner</div>
+            </div>
+            <div className="dayColumn">Tuesday</div>
+            <div className="dayColumn">Wednesday</div>
+            <div className="dayColumn">Thursday</div>
+            <div className="dayColumn">Friday</div>
+            </div>
         </div>
     )
 }
